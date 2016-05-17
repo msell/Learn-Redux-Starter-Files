5dce4dbe75edbdb7e9c5c0dda9441ee8de977af7
Matt Sell <mwsell@gmail.com>
2016-05-16 20:53:34 -0500

    convert to functional component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-const Single = React.createClass({
-  render() {
-    return (
-      <div className="single-photo">
-      I'm the single
-      </div>
-    )
-  }
-});
-
-export default Single;
+export default () => (
+  <div className="single-photo">
+  I'm the single
+  </div>
+)
